8afc9d4fe805ef9908725ba244c8886b37dec526
christy <christyyu2003@gmail.com>
2023-10-13 16:34:43 -0700

    Revert "wrote maketoken(), added a few TODO comments, added error print statements where 1. checking for second decimal, and 2. at the end of big while loop"
    This reverts commit f8bcdec4b2a243472119c11c64dad33018f0f2aa.

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,28 +1,20 @@
 #include "lib/lex.h"
 
 void createend(std::vector<token>& inputvec){
-    //TODO: make END token
-    token end;
-    //end.line = line; 
-    //end.column = column; 
-    end.text = "END";
+
 }
 void printtokens(std::vector<token>& inputvec){
-//TODO print token line, column, value
+
 }
-token maketoken(std::string text, int line, int column){
-  token t;
-  t.line = line;
-  t.column = column;
-  t.text = text;
+token maketoken(std::string i, int line, int collumn){
 
 }
 
 std::vector<token> lex() {
     char i;
     std::vector<token> output;
-    int line = 1; // TODO: iterate this at every new line
-    int column = 1; // TODO: iterate this for each new char
+    int line = 1;
+    int column = 1;
     std::string placeholder = "/0";
     std::set<char> valid {
         ')',
@@ -35,13 +27,11 @@ std::vector<token> lex() {
     while (std::cin >> i){
         if (!isspace(i)){
 
-            //if valid non-number input:
             if (valid.find(i)!=valid.end()){
                 placeholder = i;
                 output.push_back(maketoken(placeholder,line,column));
             }
 
-            //if number input check validity:
             else if(isdigit(i)){
                 placeholder = "/0";
                 bool decimal = false;
@@ -49,7 +39,7 @@ std::vector<token> lex() {
                 while (std::cin >> i && (isdigit(i) || i == '.')){
                     if (i == '.') {
                         if (decimal){
-                            //print error
+                            //error
                         }
                         else {
                             decimal = true;
@@ -59,10 +49,7 @@ std::vector<token> lex() {
                 }
                 output.push_back(maketoken(placeholder,line,column));
             }
-            //not valid!
-            else {
-                std::cout << "Syntax error on line " << line << "column " << column << ".";
-            }
+
         }
     }
 

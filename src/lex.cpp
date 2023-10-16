3d1a1efa9db2661cbbbdc0185f265652c9b831f4
Chloe Andersen <chloeandersen@ucsb.edu>
2023-10-15 21:06:24 -0700

    worked on lex.cpp: added Lexer class to organize + make lexer-built queue accessible for parser. Added end token and print token implementation. Cleaned up makeToken with token.h constructor from parse branch. Fixed error messages / runtime_error confusion.

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,28 +1,43 @@
 #include "lib/lex.h"
+#include "lib/token.h"
 
-void createend(std::queue<token>& inputq){
-    //TODO: make END token
-    token end;
-    //end.line = line; 
-    //end.column = column; 
-    end.text = "END";
-}
-void printtokens(std::queue<token>& inputq){
-//TODO print token line, column, value
-}
-token maketoken(std::string text, int line, int column){
-  token t;
-  t.line = line;
-  t.column = column;
-  t.text = text;
+
+void Lexer::createEnd(std::queue<token>& inputq, int line, int column) {
+    inputq.push(token(line, column, "END"));
 }
 
-std::queue<token> lex() {
-    char i;
-    std::queue<token> output;
+
+
+void Lexer::printTokens() {
+    while (!lexTokens.empty()) {
+        token printToken = lexTokens.front();
+        std::cout << std::setw(4) << printToken.line << std::setw(4) << 
+        printToken.column << std::setw(8) << printToken.text << std::endl;
+        // using example from  https://cplusplus.com/reference/iomanip/setw/ 
+        
+        lexTokens.pop();
+    }
+}
+
+
+std::queue<token> Lexer::getLexQueue() {
+    return lexTokens;
+}
+
+
+
+token Lexer::makeToken(int line, int column, std::string text) {
+  return token(line, column, text);
+}
+
+
+
+void Lexer::lex() {
     int line = 1;
     int column = 1;
-    std::string placeholder = "/0";
+    char i;
+    std::string placeholder;
+
     std::set<char> valid {
         ')',
         '(',
@@ -31,73 +46,76 @@ std::queue<token> lex() {
         '*',
         '/'
     };
-    while (std::cin >> i){
 
-        if (!isspace(i)){
+    while (std::cin.get(i)) { 
+        //std::cin.get(i) reads input, whitespace, and newlines
+        // https://cplusplus.com/reference/istream/istream/get/
 
-            //if valid non-number input:
-            if (valid.find(i)!=valid.end()){
-                placeholder = i;
-                output.push(maketoken(placeholder,line,column));
+        if (i == '\n') {
+            line++;
+            column = 0;
+        }
+
+        if (!isspace(i)) {
+
+            //if valid operator/parenthesis input, add to output queue:
+            if (valid.find(i) != valid.end()) {
+                lexTokens.push(makeToken(line,column, std::string(1, i)));
             }
 
-            //if number input check validity:
-            else if(isdigit(i)){
-                placeholder = "/0";
+            //if number input, check validity:
+            else if (isdigit(i)) {
                 bool decimal = false;
-                placeholder += i;
-                column++;
-                //concat the string of numbers
-                while (std::cin >> i && (isdigit(i) || i == '.')){
+                std::string placeholder(1, i); // this converts a char (1) to a string
+
+                // if number is a decimal:
+                while ((isdigit(std::cin.peek()) || std::cin.peek() == '.')) {
+                    // peek returns next character in the input sequence, without extracting it
+                    // https://cplusplus.com/reference/istream/istream/peek/
+
                     if (i == '.') {
-                        if (decimal){
+                        if (decimal) {
                             //print error
-                            std::cout << "Syntax error on line " << line << "column " << column << ".";
-                            throw std::runtime_error("Syntax error");
+                            std::cout << "Syntax error on line " << line << " column " << column << ".";
+                            exit(1);
                         }
                         else {
                             decimal = true;
                         }
                     }
-                    placeholder += i;
+                    char nextChar;
+                    std::cin.get(nextChar);
+                    placeholder += nextChar;
+
                     column++;
                 }
-                output.push(maketoken(placeholder,line,column));
-                //if next token valid make another token
-                if (valid.find(i)!=valid.end()){
-                    placeholder = i;
-                    output.push(maketoken(placeholder,line,column));
-                }
-                //if space break
-                else if (isspace(i)){
-                    column++;
-                    break;
-                }
+                lexTokens.push(makeToken(line,column, placeholder));
             }
-            //not valid!
+
+            // if not space, valid operator, or valid number, print error:
             else {
-                std::cout << "Syntax error on line " << line << "column " << column << ".";
-                throw std::runtime_error("Syntax error");
+                std::cout << "Syntax error on line " << line << " column " << column << ".";
+                exit(1);
             }
         }
-
-        if (i == '/n') {
-            line++;
-            column = 0;
-        }
+        // if input is space, add column:
         column++;
-    }
-
-    createend(output);
-    printtokens(output);
+    } 
+    createEnd(lexTokens, line, column);
 }
 
-int main(){
-    try{
 
+
+int main() {
+    try {
+        Lexer lexer;
+        lexer.lex();
+        lexer.printTokens();
     }
-    catch(const std::runtime_error& e){
+
+    catch(const std::exception& e) {
         return 1;
     }
+
     return 0;
 }
--- src/lib/lex.h
+++ src/lib/lex.h
@@ -1,11 +1,25 @@
 #ifndef LEX_H
 #define LEX_H
+
 #include <vector>
+#include <queue>
 #include <set>
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 #include "token.h"
-#include <queue>
 
+class Lexer {
+    public: 
+        void createEnd(std::queue<token>& inputq, int line, int column);
+        void printTokens();
+        token makeToken(int line, int column, std::string text);
+        void lex();
+        std::queue<token> getLexQueue();
 
+    private:
+        std::queue<token> lexTokens;
+
+};
 
 #endif
